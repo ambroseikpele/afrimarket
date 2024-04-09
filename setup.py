ef26e8962009d64e9a0c636dcbc8823c4e3e7619
sire-ambrose <ikpeleambroseobinna@gmail.com>
2024-04-09 11:28:53 +0100

    update

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ version='0.0.1'
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
+#Thi 
 with io.open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
